82873350569388019d0c410b1ef0ba02dbadced7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2022-09-11 09:36:05 -0700

    Initial commit

--- exp-004/Main.c
+++ exp-004/Main.c
@@ -4,3 +4,28 @@
 
 void continue_example();
 void break_example();
+
+int main() {
+
+    return 0;
+}
+
+void continue_example()
+{
+    for(int i=1; i<20; i++)
+    {
+        if(i == 13)
+            continue;
+        printf("%d\n", i);
+    }
+}
+
+void break_example()
+{
+    for(int i=1; i<20; i++)
+    {
+        if(i == 13)
+            break;
+        printf("%d\n", i);
+    }
+}
