7dab65e6340ada41578b040172ead369304820e3
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2022-09-11 09:36:48 -0700

    Login experience

--- exp-004/Main.c
+++ exp-004/Main.c
@@ -7,6 +7,46 @@ void break_example();
 
 int main() {
 
+    const char user[200] = "Shuaibu";
+    const char pass[200] = "shuaibupass123";
+    char username[200];
+    char password[200];
+    
+    printf("Enter username: ");
+    fgets(username, 200, stdin);
+    
+    printf("Enter Password: ");
+    fgets(password, 200, stdin);
+    
+    if(username == "" || password == "")
+    {
+        if(username != "" && password == "")
+        {
+            printf("%s password is required!\n", username);
+        }
+        else if(username == "" && password != "")
+        {
+            printf("Provide username first!\n");
+        }
+        else
+        {
+            printf("Username or Password should not be empty!\n");
+        }
+    }
+    else
+    {
+        if((username == user) && (password == pass))
+        {
+            printf("%s logged in successfully!\n");
+            continue_example();
+            break_example();
+        }
+        else
+        {
+            printf("Invalid! Username or Password\n");
+        }
+    }
+
     return 0;
 }
 
