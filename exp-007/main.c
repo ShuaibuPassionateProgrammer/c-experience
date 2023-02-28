d323c4641bda8480349efbd498e56be062f08572
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-02-28 07:15:47 -0800

    Initial commit

---
+++ exp-007/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("%d\n", 1);
+	printf("%c\n", 'A');
+	printf("%s\n", "Passionate Programmer");
+	printf("%ld\n", 111111111);
+	printf("%f\n", 2.5);
+	printf("%lf\n", 2.123456);
+	printf("%x\n", 0101010101);
+	
+	return 0;
+}
