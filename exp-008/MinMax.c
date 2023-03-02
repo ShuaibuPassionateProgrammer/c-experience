6329baa02f00c2078b67f03a78c9718a7b433b63
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-03-02 06:20:36 -0800

    Initial commit

---
+++ exp-008/MinMax.c
@@ -0,0 +1,138 @@
+#include <stdlib.h>
+#include <stdlib.h>
+#defiine NEWLINE '\n'
+
+int max(int a, int b);
+int max(int a, int b, int c);
+int max(int a, int b, int c, int d);
+int max(int a, int b, int c, int d, int e);
+int max(int a, int b, int c, int d, int e, int f);
+int max(int a, int b, int c, int d, int e, int f, int g);
+int max(int a, int b, int c, int d, int e, int f, int g, int h);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y);
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y, int z);
+
+
+int main()
+{
+
+
+    return 0;
+}
+
+int max(int a, int b);
+{
+    //
+}
+int max(int a, int b, int c)
+{
+    //
+}
+int max(int a, int b, int c, int d)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y)
+{
+    //
+}
+int max(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y, int z)
+{
+    //
+}
